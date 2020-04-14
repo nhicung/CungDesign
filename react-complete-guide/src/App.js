945dc3797567f69d6ad884e6e01b67d97a6a2e80
Nhi Cung <nhcung@knox.edu>
2020-04-13 19:36:31 -0500

    set up nav bar

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { NavigationBar } from './components/NavigationBar';
 
 class App extends Component {
   
-  
-
   render() {
     return (
       <React.Fragment>
