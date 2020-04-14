1858ba746cd7609083b80b64af75e51514c67bca
Nhi Cung <nhcung@knox.edu>
2020-04-13 19:23:18 -0500

    updated

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,47 +1,20 @@
 import React, { Component } from 'react';
-import './App.css';
-import UserInput from './Pictures/UserInput';
-import Validation from './Validation/Validation';
-import CharComponent from './CharComponent/CharComponent';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
 
 class App extends Component {
+  
+  
 
-  state = {
-      Username: '',
-  }
-
-  userNameChangedHandler = (event) => {
-    this.setState ( {Username: event.target.value} );
-  }
-
-  deleteHandler = (index) => {
-    const text = this.state.Username.split('');
-    text.splice(index,1);
-    const updatedtext = text.join('')
-    this.setState({Username:updatedtext});
-  }
-
-    render() {
-      const charList= this.state.Username.split('').map( (ch,index) => {
-        return <CharComponent 
-        character = {ch}
-        key = {index}
-        clicked = {this.deleteHandler}/>
-      });
-
-      return (
-        <div className="App">
-          <h1> Trying a new app</h1>
-          Input <UserInput
-          changed = {this.userNameChangedHandler }
-          name = {this.state.Username}/>
-          <Validation
-          Inputlength={this.state.Username.length}/>
-          {charList}
-        </div>
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <NavigationBar />
+        </Router>
+      </React.Fragment>
     );
-
-    // return React.createElement('div',{className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
 }
 
