395110e884f2ae06a9e2c0a2a7f0303b2ced717d
Nhi Cung <nhcung@knox.edu>
2020-04-15 13:39:37 -0500

    update image component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,19 +2,17 @@ import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import { NavigationBar } from './components/NavigationBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   
   render() {
-    return (
-      <div>
-        <img src="golf.jpg" alt="golff" />
-      <React.Fragment>
-        <Router>
-          <NavigationBar />
-        </Router>
-      </React.Fragment>
-    </div>
+    return (   
+    <React.Fragment>
+      <Router>
+        <NavigationBar />
+      </Router>
+    </React.Fragment>
     );
   }
 }
--- react-complete-guide/src/components/NavigationBar.js
+++ react-complete-guide/src/components/NavigationBar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
+import Logo from './Images/logo.jpg';
+import ThemePic from './Images/hanoi.jpg';
 
 const Styles = styled.div`
-  .navbar { background-color: #665544; }
+  .navbar { background-color: white; }
   a, .navbar-nav, .navbar-light .nav-link {
     font-size: 1.1em;
-    color: white;
+    color: #665544;
+    padding-right: 40px;
     padding-left: 40px;
     &:hover { color: #d4d0c6; }
   }
@@ -19,18 +22,30 @@ const Styles = styled.div`
 export const NavigationBar = () => (
   <Styles>
     <Navbar expand="lg">
-      {/* <Navbar.Brand href="/">Tutorial</Navbar.Brand> */}
+      <Navbar.Brand>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="m-auto">
-          <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
-          <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
-          <Nav.Item><Nav.Link href="/">Gallery</Nav.Link></Nav.Item> 
-          <Nav.Item><Nav.Link href="/">Workshop</Nav.Link></Nav.Item> 
-          <Nav.Item><Nav.Link href="/">Products</Nav.Link></Nav.Item> 
-          <Nav.Item><Nav.Link href="/">Inspiration</Nav.Link></Nav.Item> 
+            <Nav.Item><Nav.Link href="/"><img 
+            src={Logo}
+            width="60"
+            height="60"
+            alt="logo"/></Nav.Link></Nav.Item> 
+            
+            <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
+            <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="/">Gallery</Nav.Link></Nav.Item> 
+            <Nav.Item><Nav.Link href="/">Workshop</Nav.Link></Nav.Item> 
+            <Nav.Item><Nav.Link href="/">Products</Nav.Link></Nav.Item> 
+            <Nav.Item><Nav.Link href="/">Inspiration</Nav.Link></Nav.Item> 
         </Nav>
       </Navbar.Collapse>
     </Navbar>
+    <img 
+        src={ThemePic}
+        width="100%"
+        height="100%"
+        alt="logo"/>
   </Styles>
 )
