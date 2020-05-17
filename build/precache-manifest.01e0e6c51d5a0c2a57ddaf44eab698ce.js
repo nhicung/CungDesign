e09000acdc579e41f4672126a34046c88c56b798
Nhi Cung <nhcung@knox.edu>
2020-05-17 03:27:19 -0500

    push new content

---
+++ build/precache-manifest.01e0e6c51d5a0c2a57ddaf44eab698ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce675b4c24ab73340ffaab6fdd674c2b",
+    "url": "/CungDesign/index.html"
+  },
+  {
+    "revision": "0c58983fc94f3a27ceed",
+    "url": "/CungDesign/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cbc0ead5c66ace97afff",
+    "url": "/CungDesign/static/css/main.2f0b291b.chunk.css"
+  },
+  {
+    "revision": "0c58983fc94f3a27ceed",
+    "url": "/CungDesign/static/js/2.07dd332b.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/CungDesign/static/js/2.07dd332b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc0ead5c66ace97afff",
+    "url": "/CungDesign/static/js/main.2d244be0.chunk.js"
+  },
+  {
+    "revision": "270ded6fe2c9b2f6eff0",
+    "url": "/CungDesign/static/js/runtime-main.8948abac.js"
+  }
+]);
