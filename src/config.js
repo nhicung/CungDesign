c862b7b74b368fe34ff23833c2ec2b5e2a8ff1b9
Nhi Cung <nhcung@knox.edu>
2020-05-23 23:17:47 -0500

    slightly change

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase';
 
 let config = {
-    apiKey: "AIzaSyA_RrviVvPsb7q6U9QmFKZ7U9Mxj3hrH-0",
     authDomain: "cung-design-web.firebaseapp.com",
     databaseURL: "https://cung-design-web.firebaseio.com/",
     projectId: "cung-design-web",
