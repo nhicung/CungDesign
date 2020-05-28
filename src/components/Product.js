9b1bed24fe29a108775dc7e69d2a0478567b5cd6
Nhi Cung <nhcung@knox.edu>
2020-05-28 06:35:43 -0500

    Fix padding on Product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,12 +5,15 @@ import styled from 'styled-components';
 import tileData from './otherImages';
 
 const Styles = styled.div`
+	.root {
+		padding: 80px;
+	}
 	.card-deck{
-		width: 100%;
-		height: 50%;
-		padding: 100px;
-		color: #665544;
-		margin: auto;
+	width: 100%;
+	height: 50%;
+	color: #665544;
+	margin: auto;
+	padding-top: 40px;
 	}
 	.frame {
     height: 100%,
@@ -28,124 +31,126 @@ const Styles = styled.div`
 
 export const Product = () => (
 	<Styles>
-		<CardDeck>
-			<Card>
-				<div className = 'frame'>
-					<Card.Img 
-						src={tileData[1].img}
-						alt={tileData[1].title} />
-				</div>
-				<Card.Body>
-					<Card.Title>{tileData[1].title}</Card.Title>
-					<Card.Text>
-						This is a wider card with supporting text below as a natural lead-in to
-						additional content. This content is a little bit longer.
-				</Card.Text>
-				</Card.Body>
-			</Card>
-			<Card>
-				<div className = 'frame'>
-					<Card.Img variant='top' 
-						src={tileData[2].img}
-						alt={tileData[2].title} />
-					</div>
-				<Card.Body>
-					<Card.Title>{tileData[2].title}</Card.Title>
-					<Card.Text>
-						This is a wider card with supporting text below as a natural lead-in to
-						additional content. This content is a little bit longer.
-				</Card.Text>
-				</Card.Body>
-			</Card>
-			<Card>
-				<div className = 'frame'>
-					<Card.Img variant='top'
-						src={tileData[3].img}
-						alt={tileData[3].title} />
-				</div>
-				<Card.Body>
-					<Card.Title>{tileData[3].title}</Card.Title>
-					<Card.Text>
-						This is a wider card with supporting text below as a natural lead-in to
-						additional content. This content is a little bit longer.
-				</Card.Text>
-				</Card.Body>
-			</Card>
-			<Card>
-				<div className = 'frame'>
-					<Card.Img variant='top'
-						src={tileData[4].img}
-						alt={tileData[4].title} />
-				</div>
-				<Card.Body>
-					<Card.Title>{tileData[4].title}</Card.Title>
-					<Card.Text>
-						This is a wider card with supporting text below as a natural lead-in to
-						additional content. This content is a little bit longer.
-				</Card.Text>
-				</Card.Body>
-			</Card>
-			</CardDeck>
+		<div className= 'root'>
 			<CardDeck>
-			<Card>
-				<div className = 'frame'>
-					<Card.Img variant='top'
-						src={tileData[5].img}
-						alt={tileData[5].title} />
-				</div>
-				<Card.Body>
-					<Card.Title>{tileData[5].title}</Card.Title>
-					<Card.Text>
-						This is a wider card with supporting text below as a natural lead-in to
-						additional content. This content is a little bit longer.
-				</Card.Text>
-				</Card.Body>
-			</Card>
-			<Card>
-				<div className = 'frame'>
-					<Card.Img variant='top'
-						src={tileData[12].img}
-						alt={tileData[12].title} />
-				</div>
-				<Card.Body>
-					<Card.Title>{tileData[12].title}</Card.Title>
-					<Card.Text>
-						This is a wider card with supporting text below as a natural lead-in to
-						additional content. This content is a little bit longer.
+				<Card>
+					<div className = 'frame'>
+						<Card.Img 
+							src={tileData[1].img}
+							alt={tileData[1].title} />
+					</div>
+					<Card.Body>
+						<Card.Title>{tileData[1].title}</Card.Title>
+						<Card.Text>
+							This is a wider card with supporting text below as a natural lead-in to
+							additional content. This content is a little bit longer.
 					</Card.Text>
-				</Card.Body>
-			</Card>
-
-			<Card>
-				<div className = 'frame'>
-					<Card.Img variant='top'
-						src={tileData[10].img}
-						alt={tileData[10].title} />
-				</div>
-				<Card.Body>
-					<Card.Title>{tileData[10].title}</Card.Title>
-					<Card.Text>
-						This card has supporting text below as a natural lead-in to additional
-						content.{' '}
+					</Card.Body>
+				</Card>
+				<Card>
+					<div className = 'frame'>
+						<Card.Img variant='top' 
+							src={tileData[2].img}
+							alt={tileData[2].title} />
+						</div>
+					<Card.Body>
+						<Card.Title>{tileData[2].title}</Card.Title>
+						<Card.Text>
+							This is a wider card with supporting text below as a natural lead-in to
+							additional content. This content is a little bit longer.
 					</Card.Text>
-				</Card.Body>
-			</Card>
+					</Card.Body>
+				</Card>
+				<Card>
+					<div className = 'frame'>
+						<Card.Img variant='top'
+							src={tileData[3].img}
+							alt={tileData[3].title} />
+					</div>
+					<Card.Body>
+						<Card.Title>{tileData[3].title}</Card.Title>
+						<Card.Text>
+							This is a wider card with supporting text below as a natural lead-in to
+							additional content. This content is a little bit longer.
+					</Card.Text>
+					</Card.Body>
+				</Card>
+				<Card>
+					<div className = 'frame'>
+						<Card.Img variant='top'
+							src={tileData[4].img}
+							alt={tileData[4].title} />
+					</div>
+					<Card.Body>
+						<Card.Title>{tileData[4].title}</Card.Title>
+						<Card.Text>
+							This is a wider card with supporting text below as a natural lead-in to
+							additional content. This content is a little bit longer.
+					</Card.Text>
+					</Card.Body>
+				</Card>
+				</CardDeck>
+				<CardDeck>
+				<Card>
+					<div className = 'frame'>
+						<Card.Img variant='top'
+							src={tileData[5].img}
+							alt={tileData[5].title} />
+					</div>
+					<Card.Body>
+						<Card.Title>{tileData[5].title}</Card.Title>
+						<Card.Text>
+							This is a wider card with supporting text below as a natural lead-in to
+							additional content. This content is a little bit longer.
+					</Card.Text>
+					</Card.Body>
+				</Card>
+				<Card>
+					<div className = 'frame'>
+						<Card.Img variant='top'
+							src={tileData[12].img}
+							alt={tileData[12].title} />
+					</div>
+					<Card.Body>
+						<Card.Title>{tileData[12].title}</Card.Title>
+						<Card.Text>
+							This is a wider card with supporting text below as a natural lead-in to
+							additional content. This content is a little bit longer.
+						</Card.Text>
+					</Card.Body>
+				</Card>
 
-			<Card>
-				<div className = 'frame'>
-					<Card.Img variant='top'
-						src={tileData[6].img}
-						alt={tileData[6].title} />
-				</div>
-				<Card.Body>
-					<Card.Title>{tileData[6].title}</Card.Title>
-					<Card.Text>
-						This is a wider card with supporting text below as a natural lead-in to
-						additional content. This card has even longer content than the first to
-						show that equal height action.
-				</Card.Text>
-				</Card.Body>
-			</Card>
-		</CardDeck>
+				<Card>
+					<div className = 'frame'>
+						<Card.Img variant='top'
+							src={tileData[10].img}
+							alt={tileData[10].title} />
+					</div>
+					<Card.Body>
+						<Card.Title>{tileData[10].title}</Card.Title>
+						<Card.Text>
+							This card has supporting text below as a natural lead-in to additional
+							content.{' '}
+						</Card.Text>
+					</Card.Body>
+				</Card>
+
+				<Card>
+					<div className = 'frame'>
+						<Card.Img variant='top'
+							src={tileData[6].img}
+							alt={tileData[6].title} />
+					</div>
+					<Card.Body>
+						<Card.Title>{tileData[6].title}</Card.Title>
+						<Card.Text>
+							This is a wider card with supporting text below as a natural lead-in to
+							additional content. This card has even longer content than the first to
+							show that equal height action.
+					</Card.Text>
+					</Card.Body>
+				</Card>
+			</CardDeck>
+		</div>
 	</Styles>
 )
